60deb8350143aea8a6ba2bcd56d2fce500852b20
Ariel Jericko Gacilo <arieljericko@kasamadigital.com>
2025-05-16 17:25:31 +0800

    05162025 database set to local

--- api/config/database.php
+++ api/config/database.php
@@ -1,15 +1,14 @@
 <?php
   // Database credentials
-  // $host = 'localhost';  // Database host
-  // $username = 'root';  // Database username
-  // $password = '';  // Database password (empty if no password set)
-  // $dbname = 'exam-bank';  // Database name
+  $host = 'localhost';  // Database host
+  $username = 'root';  // Database username
+  $password = '';  // Database password (empty if no password set)
+  $dbname = 'exam-bank';  // Database name
 
-
-  $host = '151.106.122.2';  // Database host
-  $username = 'u954740893_exambank';  // Database username
-  $password = 'P4]D8$^:g*5';  // Database password (empty if no password set)
-  $dbname = 'u954740893_main';  // Database name
+  // $host = '151.106.122.2';  // Database host
+  // $username = 'u954740893_exambank';  // Database username
+  // $password = 'P4]D8$^:g*5';  // Database password (empty if no password set)
+  // $dbname = 'u954740893_main';  // Database name
   
   $conn = new mysqli($host, $username, $password, $dbname);
 
